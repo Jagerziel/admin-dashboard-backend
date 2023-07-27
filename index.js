a53cc37b0e7738b040eb1c9b524c8014dd556c77
Jagerziel <fromnytosg@gmail.com>
2023-07-27 15:29:35 -0400

    Updated backend - replacing previous repo

---
+++ index.js
@@ -0,0 +1,49 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import generalRoutes from './routes/general.js'
+import clientRoutes from './routes/client.js'
+import salesRoutes from './routes/sales.js'
+import managementRoutes from './routes/management.js'
+
+// CONFIGURATIONS
+// Congigure Env File
+dotenv.config()
+
+// Use Instance of Express
+const app = express()
+app.use(express.json())
+// Use for implementing CORS - needed to make API calls from another server
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy( { policy: "cross-origin" } ))
+// Use Morgan - Morgan is another HTTP request logger middleware for Node. js. It simplifies the process of logging requests to your application.
+app.use(morgan("common"))
+// Use Body Parser - Express body-parser is an npm module used to process data sent in an HTTP request body. It provides four express middleware for parsing JSON, Text, URL-encoded, and raw data sets over an HTTP request body.
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded( { extended: false } ))
+// Use CORS to allow cross origin requests
+app.use(cors())
+
+// ROUTES
+app.use("/client", clientRoutes) //Client Facing Routes
+app.use("/general", generalRoutes) // Gets the Users and the Dashboard
+app.use("/management", managementRoutes) //Management Routes 
+app.use("/sales", salesRoutes) // Sales Routes
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 9000
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+       app.listen(PORT, () => console.log(`Server Port: ${PORT}`))  
+    })
+    .catch((error) => console.log(`${error} did not connect`))
+
+
---
+++ routes/general.js
@@ -0,0 +1,6 @@
+import express from 'express'
+
+const router = express.Router()
+const generalRoutes = router
+export default generalRoutes
+
