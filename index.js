79a7d1af93ed6f0845a16dfba3284c7784df2588
Jagerziel <fromnytosg@gmail.com>
2023-08-01 13:12:16 -0400

    updated Transaction model and seeded data to mongodb

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ import managementRoutes from './routes/management.js'
 import User from "./models/User.js"
 import Product from './models/Product.js'
 import ProductStat from './models/ProductStat.js'
-import { dataUser , dataProduct , dataProductStat }from "./data/index.js"
+import Transaction from './models/Transaction.js'
+import { dataUser , dataProduct , dataProductStat , dataTransaction }from "./data/index.js"
 
 // CONFIGURATIONS
 // Congigure Env File
@@ -55,6 +56,7 @@ mongoose
         // User.insertMany(dataUser)
         // Product.insertMany(dataProduct)
         // ProductStat.insertMany(dataProductStat)
+        // Transaction.insertMany(dataTransaction)
     })
     .catch((error) => console.log(`${error} did not connect`))
 
